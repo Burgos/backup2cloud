f06885ad93567dc3d937b156cab9ae7a0cfa2f47
Nemanja Boric <nemanja@nemanjaboric.com>
2019-09-29 19:21:07 +0200

    Add dummy main.
    This adds dummy main method. In the future to be replaced with the
    unittests.

--- src/backup2cloud.cpp
+++ src/backup2cloud.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
 #include "backend/traverser.hpp"
 
-int main() {
+namespace fs = std::filesystem;
+
+int main(int argc, char** argv) {
+    for (int i = 1; i < argc; i++) {
+        for (const auto& fh: calculateHashes(argv[i])) {
+            std::cout << fh.file_path << "\t" << fh.hash << std::endl;
+            std::flush(std::cout);
+        }
+    }
     return 0;
 } 
