ee308874e05e1b612881e03fa8a6c855a570aa31
Nemanja Boric <nemanja@nemanjaboric.com>
2019-09-29 23:02:32 +0200

    Store the backup state into the sqlite database
    This stores the backup state into the sqlite database to keep
    track of the backup.

--- src/backup2cloud.cpp
+++ src/backup2cloud.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <SQLiteCpp/SQLiteCpp.h>
 #include "backend/traverser.hpp"
+#include "session/backup_session.hpp"
 
 namespace fs = std::filesystem;
 
+void initializeDatabase(SQLite::Database& db) {
+    SQLite::Statement create_tables{db, "CREATE TABLE backup_files (backup_name TEXT, filename TEXT, filehash TEXT)"};
+    create_tables.exec();
+    SQLite::Statement create_index{db, "CREATE UNIQUE INDEX file_index ON backup_files(backup_name, filename)"};
+    create_index.exec();
+}
+
 int main(int argc, char** argv) {
-    for (int i = 1; i < argc; i++) {
-        for (const auto& fh: calculateHashes(argv[i])) {
-            std::cout << fh.file_path << "\t" << fh.hash << std::endl;
-            std::flush(std::cout);
-        }
+    SQLite::Database db("files.sql", SQLite::OPEN_READWRITE);
+    if (!db.tableExists("backup_files")) {
+        initializeDatabase(db);
+    }
+    std::string backup_name(argv[1]);
+    for (int i = 2; i < argc; i++) {
+        auto files = listFilesForBackup(backup_name, argv[i], db);
+        performBackup(backup_name, files, db, [](const std::string& path) {
+            std::cout << "Backing up " << path << "\n";
+        });
     }
     return 0;
 } 
---
+++ src/session/backup_session.hpp
@@ -0,0 +1,59 @@
+#ifndef BACKUP2CLOUD_BACKEND_BACKUP_SESSION_H
+#define BACKUP2CLOUD_BACKEND_BACKUP_SESSION_H
+
+
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <filesystem>
+#include <cassert>
+
+namespace fs = std::filesystem;
+
+/// Defines a backup session for the directory.
+/// Goes over a directory and compares if the files are changed, and
+/// runs a scheduled work.
+std::vector<FileWithHash> listFilesForBackup (const std::string& backup_name, const fs::path& root, SQLite::Database& db) {
+    std::vector<FileWithHash> affected_files;
+    auto files = calculateHashes(root);
+    SQLite::Statement query{db, "SELECT count(*) as num_matches FROM backup_files WHERE backup_name = :backup_name AND filename = :filename AND filehash = :filehash"};
+    query.bind(":backup_name", backup_name);
+    for (const auto& fh: files) {
+        // query the db
+        query.reset();
+        query.bind(":filename", fh.file_path);
+        query.bind(":filehash", fh.hash);
+        int num_matches = 0;
+        if (query.executeStep() == true) {
+            num_matches = query.getColumn("num_matches").getInt();
+        }
+
+        switch (num_matches) {
+            case 0:
+                // backup this one.
+                affected_files.push_back(fh);
+                break;
+            case 1:
+                // skip, found a match
+                break;
+            default:
+                throw std::runtime_error("Several hashes for the same filename found.");
+                break;
+        }
+    }
+    return affected_files;
+}
+
+template <typename BackupFunction>
+void performBackup (const std::string& backup_name, std::vector<FileWithHash> filesToBackup, SQLite::Database& db, BackupFunction backup) {
+    SQLite::Statement query{db, "INSERT OR REPLACE INTO backup_files(backup_name, filename, filehash) VALUES (:backup_name, :filename, :filehash)"};
+    query.bind(":backup_name", backup_name);
+    for(const auto& fh: filesToBackup) {
+        backup(fh.file_path);
+        std::cout << fh.file_path << " " << fh.hash << std::endl;
+        query.reset();
+        query.bind(":filename", fh.file_path);
+        query.bind(":filehash", fh.hash);
+        query.exec();
+    }
+}
+
+#endif
